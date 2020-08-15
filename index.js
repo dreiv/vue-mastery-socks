977795357a087527617037eb7f0d614b52684bab
Andrei Voicu <dreivoicu@gmail.com>
2020-08-15 18:36:48 +0300

    v-for

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    image: './assets/vmSocks-green.png'
+    image: './assets/vmSocks-green.png',
+    altText: "A pair of socks",
+    inventory: 100,
+    details: [
+      '80% cotton',
+      '20% polyester',
+      'Gender-neutral'
+    ],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: './assets/vmSocks-green.png',
+        variantQuantity: 10
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: './assets/vmSocks-blue.png',
+        variantQuantity: 0
+      }
+    ],
   }
 });
